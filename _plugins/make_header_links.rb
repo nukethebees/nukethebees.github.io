1c268ac99a6260cb394be7a4b8b60c97afb56825
Matthew Love <matthewlove94@gmail.com>
2026-04-05 23:56:36 +0100

    Merge the lists

---
+++ _plugins/make_header_links.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  class CategoryHeaderGenerator < Generator
+    safe true
+    priority :lowest
+
+    def generate(site)
+      manual_links = site.config['header_links'] || []
+      category_pages = site.config['category_pages'] || []
+      category_links = category_pages.map do |cat|
+        {
+          'title' => cat['title'],
+          'url'   => cat['permalink']
+        } || []
+      end
+      site.config['header_links'] = manual_links + category_links
+    end
+  end
+end
